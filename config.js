4cee2a42de38a8cbf1cb621691d6114ffaaef46a
lg221 <liverboy233@gmail.com>
2025-06-19 19:29:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'M6E3WTra#6FrX5bsnu7XVJwUcT7NouPV79msk5C2hV14jzuXugxs',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    AUTO_VOICE:"true"
+    AUTO_VOICE:"true",
+    JID: process.env.JID || "120363417168743361@newsletter",
+    FOOTER: 'ලස්සන සිංදු 💗🇱🇰',
 };
