67cead1c58fd8597ac90dbd6dd5886b2429c21e9
lg221 <liverboy233@gmail.com>
2025-06-20 08:17:23 +0530

    Update imgurl.js

--- plugins/imgurl.js
+++ plugins/imgurl.js
@@ -45,12 +45,12 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const channelMeta = await conn.newsletterMetadata("invite", channelId);
         await conn.newsletterReactMessage(channelMeta.id, messageId, emoji);
 
-        return reply(`╭━━━〔 *NEXUS-XMD* 〕━━━┈⊷
+        return reply(`╭━━━〔 *VISHWA-MD* 〕━━━┈⊷
 ┃▸ *Success!* Reaction sent
 ┃▸ *Channel:* ${channelMeta.name}
 ┃▸ *Reaction:* ${emoji}
 ╰────────────────┈⊷
-> *© Pᴏᴡᴇʀᴇᴅ Bʏ PKDRILLER*`);
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴠɪꜱʜᴡᴀ-ᴍᴅ🎭*`);
     } catch (e) {
         console.error(e);
         reply(`❎ Error: ${e.message || "Failed to send reaction"}`);
