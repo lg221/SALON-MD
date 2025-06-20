b9002a33e47034393ed2ebce7d68bb55f555c321
lg221 <liverboy233@gmail.com>
2025-06-20 10:21:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'vishwa~M6E3WTra#6FrX5bsnu7XVJwUcT7NouPV79msk5C2hV14jzuXugxs',
+    SESSION_ID: process.env.SESSION_ID || 'M6E3WTra#6FrX5bsnu7XVJwUcT7NouPV79msk5C2hV14jzuXugxs',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     AUTO_VOICE:"true",
     JID: process.env.JID || "120363417168743361@newsletter",
